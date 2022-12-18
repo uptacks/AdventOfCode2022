2771225dda4190b6a89de51734a32ca61f6e86d6
uptack <uptack@protonmail.com>
2022-12-18 13:17:07 +0000

    Rebase

---
+++ src/days/day5.rs
@@ -0,0 +1,19 @@
+use itertools::Itertools;
+
+use crate::days::{AdventSolution, Day};
+
+impl AdventSolution for Day<5> {
+    type OutputOne = u64;
+    type OutputTwo = u64;
+
+    fn problem_one(input: &str) -> Self::OutputOne {
+        
+    return 0;
+    }
+
+    fn problem_two(input: &str) -> Self::OutputTwo {
+    return 0;
+
+    }
+
+}
